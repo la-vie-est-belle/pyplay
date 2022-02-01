e5c107a8a9aac8ffab12416b14353976d12eacb2
la vie <louasure@126.com>
2022-02-01 12:32:23 +0800

    assets window

--- Window/WindowParts/AssetWindow.py
+++ Window/WindowParts/AssetWindow.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
@@ -10,66 +11,140 @@ class AssetWindow(QWidget):
     pathSignal = pyqtSignal(str)
 
     def __init__(self):
-        super(AssetWindow, self).__init__()
-        self._projectDirModel = QFileSystemModel()
-        self._projectTreeView = QTreeView(self)
-        self._projectPath = '/Users/louis/Desktop/pyplay'
-        self._contextMenu = ContextMenu(self)
+        super(QWidget, self).__init__()
 
-        self._main()
+        self.projectTreeView = QTreeView()
+        self.projectDirModel = QFileSystemModel()
+        self.projectPath = '/Users/louis/Desktop/pyplay'
+        self.contextMenu = AssetWidnowContextMenu(self)
 
-    def _main(self):
-        self._initWidgets()
-        self._initSignals()
-        self._initLayouts()
+        self.currentIndex = None
 
-    def _initWidgets(self):
+        self.main()
+
+    def main(self):
+        self.initWindowAttrs()
+        self.initWidgets()
+        self.initSignals()
+        self.initLayouts()
+
+    def initWindowAttrs(self):
+        self.setWindowTitle('资源管理器')
+
+    def initWidgets(self):
         # Show the current project directory.
-        index = self._projectDirModel.setRootPath(self._projectPath)
-        self._projectTreeView.setModel(self._projectDirModel)
-        self._projectTreeView.setRootIndex(index)
+        index = self.projectDirModel.setRootPath(self.projectPath)
+        self.projectTreeView.setModel(self.projectDirModel)
+        self.projectTreeView.setRootIndex(index)
+        self.projectTreeView.setExpandsOnDoubleClick(True)
 
         # Only need to show file(folder) name.
-        self._projectTreeView.setColumnHidden(1, True)
-        self._projectTreeView.setColumnHidden(2, True)
-        self._projectTreeView.setColumnHidden(3, True)
+        self.projectTreeView.setColumnHidden(1, True)
+        self.projectTreeView.setColumnHidden(2, True)
+        self.projectTreeView.setColumnHidden(3, True)
 
         # Don't want to show the header.
-        self._projectTreeView.setHeaderHidden(True)
+        self.projectTreeView.setHeaderHidden(True)
 
-    def _initSignals(self):
-        self.pathSignal.connect(self._setProjectPath)
+    def initSignals(self):
+        self.pathSignal.connect(self.setProjectPath)
+        self.projectTreeView.doubleClicked.connect(self.openFile)
 
-    def _initLayouts(self):
+    def initLayouts(self):
         hLayout = QHBoxLayout(self)
-        hLayout.addWidget(self._projectTreeView)
+        hLayout.setContentsMargins(0, 0, 0, 0)
+        hLayout.addWidget(self.projectTreeView)
 
     """Slots"""
-    def _setProjectPath(self, path):
-        self._projectPath = path
+
+    def openFile(self, modelIndex):
+        path = self.projectDirModel.filePath(modelIndex)
+        if os.path.isfile(path):
+            print('打开')
+
+    def setProjectPath(self, path):
+        self.projectPath = path
 
     """Events"""
     def contextMenuEvent(self, event):
-        self._contextMenu.exec(event.globalPos())
+        self.contextMenu.exec(event.globalPos())
+
+        # index = self.projectTreeView.indexAt(event.pos())
+
+        # # User clicks on the blank
+        # if not index.isValid():
+        #     self.contextMenu.showContextMenuForBlank(event.globalPos())
+        #     return
+
+        # filePath = self.projectDirModel.filePath(index)
+        # print(filePath)
+        # # User clicks on the folder
+        # if os.path.isdir(filePath):
+        #     print(1)
+        #     self.contextMenu.showContextMenuForFolder(event.globalPos())
+        # # User clicks on the file
+        # else:
+        #     print(2)
+        #     self.contextMenu.showContextMenuForFile(event.globalPos())
+
+    def mousePressEvent(self, event):
+        # if event.button() == Qt.RightButton:
+        #     index = self.indexAt(event.pos())
+        #
+        #     if index.isValid():
+        #         print(1)
+        #     else:
+        #         print(2)
+        ...
 
 
-class ContextMenu(QMenu):
+class AssetWidnowContextMenu(QMenu):
     def __init__(self, parent):
-        super(ContextMenu, self).__init__(parent=parent)
-        newFileMenu = QMenu(self)
-        newFileMenu.setTitle('新建文件')
-        newTxtFileAction = QAction('新建txt文件', newFileMenu)
-        newPythonFileAction = QAction('新建Py文件', newFileMenu)
-        newJsonFileAction = QAction('新建JSON文件', newFileMenu)
-        newFileMenu.addAction(newTxtFileAction)
-        newFileMenu.addAction(newPythonFileAction)
-        newFileMenu.addAction(newJsonFileAction)
+        super(AssetWidnowContextMenu, self).__init__(parent=parent)
+        """Submenu"""
+        self.newFileMenu = QMenu(self)
+        self.newFileMenu.setTitle('新建文件')
 
-        newFolderAction = QAction('新建文件夹', self)
-        deleteAction = QAction('删除', self)
-        self.addAction(newFolderAction)
-        self.addMenu(newFileMenu)
-        self.addAction(deleteAction)
+        """Action"""
+        # Submenu Actions
+        self.newTxtFileAction = QAction('新建txt文件', self.newFileMenu)
+        self.newPythonFileAction = QAction('新建Py文件', self.newFileMenu)
+        self.newJsonFileAction = QAction('新建JSON文件', self.newFileMenu)
+
+        # Main menu Actions
+        self.newFolderAction = QAction('新建文件夹', self)
+        self.renameAction = QAction('重命名', self)
+        self.deleteAction = QAction('删除', self)
+        self.pasteAction = QAction('粘贴', self)
+        self.copyAction = QAction('复制', self)
+        self.cutAction = QAction('剪切', self)
+
+        self.main()
+
+    def main(self):
+        self.addSubmenu()
+        self.addSubmenuActions()
+        self.addMainMenuActions()
+
+    def addSubmenu(self):
+        self.addMenu(self.newFileMenu)
+
+    def addSubmenuActions(self):
+        self.newFileMenu.addAction(self.newTxtFileAction)
+        self.newFileMenu.addAction(self.newPythonFileAction)
+        self.newFileMenu.addAction(self.newJsonFileAction)
+
+    def addMainMenuActions(self):
+        self.addAction(self.newFolderAction)
+        self.addAction(self.renameAction)
+        self.addAction(self.deleteAction)
+        self.addAction(self.pasteAction)
+        self.addAction(self.copyAction)
+        self.addAction(self.cutAction)
+
+    def showContextMenu(self):
+        # 在这里检查是否action enabled
+        ...
 
 
 if __name__ == '__main__':
